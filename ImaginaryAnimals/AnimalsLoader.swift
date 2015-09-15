f9d5d850a586b0609b44e314d6054108e5d4bb50
James Taylor <trueheartedly@gmail.com>
2015-09-15 11:11:20 -0700

    json constructor

--- ImaginaryAnimals/AnimalsLoader.swift
+++ ImaginaryAnimals/AnimalsLoader.swift
@@ -10,34 +10,20 @@ import Foundation
 
 struct AnimalsLoader {
     func loadAnimals() -> [ImaginaryAnimal] {
-        let mermaid = ImaginaryAnimal(
-            name: "Mermaid",
-            height: 1.5,
-            location: "Oceans",
-            dateLastSeen: "1858",
-            imageUrl: NSURL(string: "https://upload.wikimedia.org/wikipedia/commons/0/09/MehganTheMermaid.jpg")
-        )
-        let lochness = ImaginaryAnimal(
-            name: "Lochness",
-            height: 11.5,
-            location: "Oceans",
-            dateLastSeen: "1778",
-            imageUrl: NSURL(string: "https://upload.wikimedia.org/wikipedia/commons/2/26/Loch_Ness_Monster_01.jpg")
-        )
-        let bigfoot = ImaginaryAnimal(
-            name: "Bigfoot",
-            height: 50,
-            location: "Woodlands",
-            dateLastSeen: "1944",
-            imageUrl: NSURL(string: "https://upload.wikimedia.org/wikipedia/commons/a/ac/BigfootStatue-SilverLakeWA.jpg")
-        )
-        let liger = ImaginaryAnimal(
-            name: "Liger Leony",
-            height: 3,
-            location: "Sahara",
-            dateLastSeen: "1998",
-            imageUrl: NSURL(string: "https://upload.wikimedia.org/wikipedia/commons/d/dd/Liger_portrait.jpg")
-        )
-    return [mermaid, lochness, bigfoot, liger]
+
+        var animalsArray = [ImaginaryAnimal]()
+        
+        guard let url = NSBundle.mainBundle().URLForResource("Animals", withExtension: "json"),
+            let data = NSData(contentsOfURL: url),
+            let jsonArray = try? NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions(rawValue: 0)) as? [AnyObject] else {
+            
+                fatalError("Couldn't load or parse file")
+        }
+        if let jsonArray:[AnyObject] = jsonArray {
+            for animalJson in jsonArray {
+                let animal = ImaginaryAnimal(fromJson: animalJson)
+            }
+        }
+        return animalsArray
     }
 }
